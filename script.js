1056ab41cea423a6cd6a8df8081fad2e56b8e860
70656E69736573 <lewisbass@vt.edu>
2024-08-17 14:19:59 -0400

    gamer mode

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ function shif(timeStamp){
     oy = document.getElementById("halo").getAttribute("cy");
     document.getElementById("halo").setAttribute("cx", (nx*0.01+ox*0.99));
     document.getElementById("halo").setAttribute("cy", (ny*0.01+oy*0.99));
+    document.getElementById("halo_color_1").setAttribute("stop-color", "hsl(" + timeStamp/200 + ", 100%, 100%)");
+    document.getElementById("halo_color_2").setAttribute("stop-color", "hsl(" + timeStamp/200 + ", 100%, 70%)");
+    document.getElementById("halo_color_3").setAttribute("stop-color", "hsl(" + timeStamp/200 + ", 100%, 57%)");
     console.log(anilock)
     if(Math.pow(ox-nx, 2) + Math.pow(oy-ny, 2) > 1){
         requestAnimationFrame(shif);
